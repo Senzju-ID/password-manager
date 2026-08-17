8b20cadee4f6958f881a728c80ff12ae289a6bdd
Zhuzhu-ID <senzjuserver@gmail.com>
2026-08-17 11:35:52 +0000

    feat: reorganize UI component exports and update index structure

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,5 @@
-export * from "./ui";
-export { default as FormForgetPassword} from "./auth/formForgetPassword";
+export * from "./ui/index";
+export { default as FormForgetPassword } from "./auth/formForgetPassword";
 export { default as FormLogin } from "./auth/formLogin";
 export { default as FormRegister } from "./auth/formRegister";
 export { default as Header } from "./layout/header";
